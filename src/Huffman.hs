3f390ef203f1df10c3e05afe7504040709edb554
Elian Babireski <55952848+babireski@users.noreply.github.com>
2024-04-11 12:59:10 -0300

    Record notation added

--- src/Huffman.hs
+++ src/Huffman.hs
@@ -1,11 +1,9 @@
-{-# LANGUAGE UnicodeSyntax #-}
-
 module Huffman where
 
 import Data.List
 import Synonyms
 
-data Huffman = Leaf Frequency Symbol | Fork Frequency Huffman Huffman deriving Show
+data Huffman = Leaf {frequency :: Frequency, symbol :: Symbol} | Fork {frequency :: Frequency, left :: Huffman, right :: Huffman} deriving Show
 
 instance Eq Huffman where
     (==) x y = frequency x == frequency y
@@ -13,36 +11,31 @@ instance Eq Huffman where
 instance Ord Huffman where
     (<=) x y = frequency x <= frequency y
 
-frequencies ∷ String → [Huffman]
-frequencies s = [Leaf (length x) (head x) | x ← group $ sort s]
+frequencies :: String -> [Huffman]
+frequencies s = [Leaf (length x) (head x) | x <- group $ sort s]
 
-frequency ∷ Huffman → Frequency
-frequency (Leaf f _)   = f
-frequency (Fork f _ _) = f
-
-merge ∷ Huffman → Huffman → Huffman
+merge :: Huffman -> Huffman -> Huffman
 merge x y = Fork (frequency x + frequency y) x y
 
-huffman ∷ [Huffman] → Huffman
+huffman :: [Huffman] -> Huffman
 huffman x = build $ sort x where
-    build ∷ [Huffman] → Huffman
-    build []      = error "Error"
+    build :: [Huffman] -> Huffman
+    build []      = error "Cannot build tree from an empty list."
     build [x]     = x
     build (x:y:r) = build $ insert (merge x y) r
 
-dictionary ∷ Huffman → [(Symbol, String)]
+dictionary :: Huffman -> [(Symbol, String)]
 dictionary (Leaf _ s)   = [(s, "")]
 dictionary (Fork _ l r) = map (('0' :) <$>) (dictionary l) ++ map (('1' :) <$>) (dictionary r)
 
-encode ∷ String → Huffman → String
+encode :: String -> Huffman -> String
 encode string huffman = maybe "" id $ concat <$> (sequence $ map (`lookup` (dictionary huffman)) string)
 
-decode ∷ String → Huffman → Maybe String
-decode string huffman = undefined
-
-search ∷ String → Huffman → (Maybe Char, String)
-search string (Leaf _ s)   = (Just s, string) 
-search []     (Fork _ _ _) = (Nothing, "")
-search (h:t)  (Fork _ l r) = if h == '0' then search t l else search t r
-
-h = huffman $ frequencies "cavalo"
+-- decode :: String -> Huffman -> Maybe String
+-- decode string huffman = maybe "" id $ sequence go string huffman
+--     go []     _       = []
+--     go string huffman = symbol : go leftover huffman where (symbol, leftover) = search string huffman where
+--         search :: String -> Huffman -> (Maybe Char, String)
+--         search cypher (Leaf _ s)   = (Just s, cypher) 
+--         search []     (Fork _ _ _) = (Nothing, "")
+--         search (h:t)  (Fork _ l r) = if h == '0' then search t l else search t r
