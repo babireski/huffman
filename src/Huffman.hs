5c8adb2c645d42c600bf790646d6fab92f22086c
babireski <elian.babireski@gmail.com>
2024-04-13 17:17:34 -0300

    Dictionary renamed to codebook

--- src/Huffman.hs
+++ src/Huffman.hs
@@ -4,7 +4,7 @@ import Data.List
 
 type Frequency = Int
 type Symbol    = Char
-data Huffman = Leaf {frequency :: Frequency, symbol :: Symbol} | Fork {frequency :: Frequency, left :: Huffman, right :: Huffman} deriving Show
+data Huffman   = Leaf {frequency :: Frequency, symbol :: Symbol} | Fork {frequency :: Frequency, left :: Huffman, right :: Huffman} deriving Show
 
 instance Eq Huffman where
     (==) x y = frequency x == frequency y
@@ -15,28 +15,27 @@ instance Ord Huffman where
 frequencies :: String -> [Huffman]
 frequencies s = [Leaf (length x) (head x) | x <- group $ sort s]
 
-merge :: Huffman -> Huffman -> Huffman
-merge x y = Fork (frequency x + frequency y) x y
-
 huffman :: [Huffman] -> Huffman
 huffman x = build $ sort x where
     build :: [Huffman] -> Huffman
     build []      = error "Cannot build tree from an empty list."
     build [x]     = x
-    build (x:y:r) = build $ insert (merge x y) r
+    build (x:y:r) = build $ insert (merge x y) r where
+        merge :: Huffman -> Huffman -> Huffman
+        merge x y = Fork (frequency x + frequency y) x y
 
-dictionary :: Huffman -> [(Symbol, String)]
-dictionary (Leaf _ s)   = [(s, "")]
-dictionary (Fork _ l r) = map (('0' :) <$>) (dictionary l) ++ map (('1' :) <$>) (dictionary r)
+codebook :: Huffman -> [(Symbol, String)]
+codebook (Leaf _ s)   = [(s, "")]
+codebook (Fork _ l r) = map (('0' :) <$>) (codebook l) ++ map (('1' :) <$>) (codebook r)
 
 encode :: String -> Huffman -> String
-encode string huffman = maybe "" id $ concat <$> (sequence $ map (`lookup` (dictionary huffman)) string)
+encode string huffman = maybe "" id $ concat <$> (sequence $ map (`lookup` (codebook huffman)) string)
 
 decode :: String -> Huffman -> String
 decode string huffman = maybe "" id $ sequence $ go string huffman where
     go :: String -> Huffman -> [Maybe Char]
     go []     _       = []
-    go cypher huffman = symbol : go leftover huffman where (symbol, leftover) = search cypher huffman
+    go string huffman = symbol : go leftover huffman where (symbol, leftover) = search string huffman
 
 search :: String -> Huffman -> (Maybe Char, String)
 search string (Leaf _ s)   = (Just s, string)
